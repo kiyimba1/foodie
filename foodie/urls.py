14bf8686e4aa2ca13547d705f225466bee2be606
kiyimba5 <edwineddie54@gmail.com>
2020-04-09 14:09:36 +0300

    created customer Api

--- foodie/urls.py
+++ foodie/urls.py
@@ -15,7 +15,36 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from foodieapp import views, apis
+from django.contrib.auth import views as auth_view
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    url(r'^manage/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url(r'^restaurant/sign-in/$', auth_view.LoginView.as_view(template_name = 'restaurant/sign_in.html'), 
+        name= 'restaurant-sign-in'),
+    url(r'^restaurant/sign-out', auth_view.LogoutView.as_view(),
+        {'next_page': '/'},
+        name='restaurant-sign-out'),
+     url(r'^restaurant/sign-up', views.restaurant_sign_up,
+        name='restaurant-sign-up'),
+    url(r'^restaurant/$', views.restaurant_home, name='restaurant-sign-out'),
+    
+    url(r'^restaurant/account/$', views.restaurant_account, name='restaurant-account'),
+    url(r'^restaurant/meal/$', views.restaurant_meal, name='restaurant-meal'),
+    url(r'^restaurant/meal/add/$', views.restaurant_add_meal, name='restaurant-add-meal'),
+    url(r'^restaurant/meal/edit/(?P<meal_id>\d+)/$', views.restaurant_edit_meal, name='restaurant-edit-meal'),
+    url(r'^restaurant/order/$', views.restaurant_order, name='restaurant-order'),
+    url(r'^restaurant/report/$', views.restaurant_report, name='restaurant-report'),
+
+    # APIs for CUSTOMERS
+    url(r'^api/customer/restaurants/$', apis.customer_get_restaurants),
+    url(r'^api/customer/meals/(?P<restaurant_id>\d+)/$', apis.customer_get_meals),
+    url(r'^api/customer/order/add/$', apis.customer_add_order),
+    url(r'^api/customer/order/latest/$', apis.customer_get_latest_order),
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ foodieapp/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+from foodieapp.models import Restaurant, Meal, Customer, Driver, Order, OrderDetails
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    logo = serializers.SerializerMethodField()
+
+    def get_logo(self, restaurant):
+        request = self.contex.get(request)
+        logo_url = restaurant.logo.url
+        return request.build_absoute_uri(logo_url)
+
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "phone", "address", "logo")
+
+class MealSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    def get_image(self, meal):
+        request = self.context.get('request')
+        image_url = meal.image.url
+        return request.build_absolute_uri(image_url)
+
+    class Meta:
+        model = Meal
+        fields = ("id", "name", "short_description", "image", "price")
+
+# Order Serializer
+class OrderCustomerSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source="user.get_full_name")
+
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "avatar", "phone", "address")
+
+class OrderDriverSerializer(serializers.ModelSerializer):
+    name = serializers.ReadOnlyField(source="user.get_full_name")
+
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "avatar", "phone", "address")
+
+class OrderRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "phone", "address")
+
+class OrderMealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = ("id", "name", "price")
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    meal = OrderMealSerializer()
+    class Meta:
+        model = OrderDetails
+        fields = ("id", "meal", "quantity", "sub_total")
+
+class OrderSerializer(serializers.ModelSerializer):
+    customer = OrderCustomerSerializer()
+    driver = OrderDriverSerializer()
+    restaurant = OrderRestaurantSerializer()
+    order_details = OrderDetailSerializer()
+    status = serializers.ReadOnlyField(source = "get_status_display")
+
+    class Meta:
+        model = Order
+        fields = ("id", "customer", "restaurant", "driver", "order_details", "total", "status", "address")
